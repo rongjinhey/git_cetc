efee5b4dfebc2cb6bc50e216ef32c9af2fa7e538
cet28 <code_jin@126.com>
2022-05-20 14:40:50 +0800

    master modify

--- Hello.java
+++ Hello.java
@@ -1,7 +1,8 @@
 public class Hello{
 public static void main(String[] args){
 System.out.println("hello!");
-System.out.println("hehhhhhh");
-System.out.println("dev");
+System.out.println("hehh");
+System.out.println("dev!!!!!!!!");
+System.out.println("modify by master branch");
 }
 }
